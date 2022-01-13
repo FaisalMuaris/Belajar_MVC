8cffff0b6e9e015934dde6c48e6289343c881ed2
Faisal Muaris <faisal.muaris.fm@gmail.com>
2022-01-13 13:57:20 +0700

    Mengatur View

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,15 +1,26 @@
 <?php
 
 
-class About
+class About extends Controller
 {
   // Method default
-  public function index($nama = 'Faisal', $pekerjaan = 'Pelajar')
+  public function index($nama = 'Faisal', $pekerjaan = 'Pelajar', $umur = 22)
   {
-    echo "Hallo, Nama saya $nama .. Saya adalah seorang $pekerjaan";
+    $data['nama'] = $nama;
+    $data['pekerjaan'] = $pekerjaan;
+    $data['umur'] = $umur;
+
+
+    $data['judul'] = 'About Me';
+    $this->view('templates/header', $data);
+    $this->view('about/index', $data);
+    $this->view('templates/footer');
   }
   public function page()
   {
-    echo ('About/page');
+    $data['judul'] = 'Pages';
+    $this->view('templates/header', $data);
+    $this->view('about/page');
+    $this->view('templates/footer');
   }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,2 @@
+<h1>About Me</h1>
+<p> Hallo Nama saya <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> Tahun. Saya seorang <?= $data['pekerjaan']; ?></p>
